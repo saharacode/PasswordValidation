408f7bdeae256c24b2848df868cda723efbf4271
saharacode <johanndallmann@gmx.de>
2023-04-21 10:32:19 +0200

    junit5 and Test-class added

--- src/main/java/de/neuefische/Main.java
+++ src/main/java/de/neuefische/Main.java
@@ -1,5 +1,12 @@
 package de.neuefische;
 
 public class Main {
+    public static void main(String[] args) {
 
+    }
+
+    public static boolean lengthPW(String pw){
+        boolean valid = false;
+        return valid;
+    }
 }
